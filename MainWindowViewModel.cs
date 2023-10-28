7eb484e527788b98f37fd2708b275acb571a4836
Filip Najdovski <filip.najdovski@outlook.com>
2023-10-28 14:02:41 +0800

    UI done

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace InvoiceApp
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        public InvoiceModel Invoice { get; set; }
+
+        public ICommand GenerateDocumentCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            Invoice = new InvoiceModel();
+            GenerateDocumentCommand = new RelayCommand(GenerateDocument);
+        }
+
+        public void GenerateDocument(object _)
+        {
+            Console.WriteLine(Invoice.Name);
+        }
+    }
+
+    public class InvoiceModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; } = DateTime.Today;
+        public decimal Cost { get; set; }
+        public decimal Price => Math.Round(Cost, 2);
+    }
+}
